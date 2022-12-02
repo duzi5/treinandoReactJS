fb701693d737c9cfba131361989184b3ab8e5f6f
Antonio Netto <60482472+duzisnetto@users.noreply.github.com>
2022-12-02 13:35:17 -0300

    apenas formatando o código

--- hooks/src/views/examples/UseState.jsx
+++ hooks/src/views/examples/UseState.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+
 
 const UseState = (props) => {
+  const [x, setx] = UseState(0)
+
+
+
     return (
         <div className="UseState">
             <PageTitle
                 title="Hook UseState"
                 subtitle="Estado em componentes funcionais!"
             />
+            <SectionTitle title="Exercício 01"/>
+            <SectionTitle title="Exercício 02"/>
         </div>
+
     )
 }
 
