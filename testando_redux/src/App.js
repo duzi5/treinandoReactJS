a97fec9a752b7a2cae2a288f6ae27d0506e21284
Antonio Netto <60482472+duzisnetto@users.noreply.github.com>
2022-12-20 12:38:20 -0300

    Evoluindo com os testes de redux

---
+++ testando_redux/src/App.js
@@ -0,0 +1,16 @@
+import Card from "./components/Card"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <Card>
+            <h1>
+              nada
+            </h1>
+        </Card>
+    </div>
+  );
+}
+
+export default App;
